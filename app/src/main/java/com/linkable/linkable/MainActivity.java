640b03f9ed7caa21fbcb8db804e7284302f00e08
김성은 <ss_eun97@naver.com>
2019-04-08 01:53:15 +0900

    코드저장 ^______^

--- app/src/main/java/com/linkable/linkable/MainActivity.java
+++ app/src/main/java/com/linkable/linkable/MainActivity.java
@@ -2,11 +2,13 @@ package com.linkable.linkable;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.InputStream;
@@ -23,43 +25,67 @@ import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView1;
-    TextView textView2;
-    ImageView imageView;
+    TextView titleTextView;
+    TextView authorTextView;
+    ImageView bookImageView;
     String imageURL;
     Bitmap bitmap;
-    Handler handler;
+    InflatedBookLayout bookLayout;
 
-    static final String URL = "http://10.91.48.96:8000/";
+    static final String URL = "http://10.91.102.85:8000/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        handler = new Handler();
-        textView1 = (TextView)findViewById(R.id.bestTitleTextView);
-        textView2 = (TextView)findViewById(R.id.bestAuthorTextView);
-        imageView = (ImageView)findViewById(R.id.bestbookImage);
-        book();
+        LinearLayout bestList = (LinearLayout)findViewById(R.id.bestList);
+
+        //bookImageView = (ImageView)findViewById(R.id.bookImage);
+        //titleTextView = (TextView)findViewById(R.id.bookTitleTextView);
+        //authorTextView = (TextView)findViewById(R.id.bookAuthorTextView);
+        imageURL = "http://image.yes24.com/momo/TopCate1833/MidCate006/183255693.jpg";
+
+        //
+
+        for (int i = 1; i <= 5; i++) {
+            bookLayout = new InflatedBookLayout(getApplicationContext());
+
+            bookImageView = (ImageView)bookLayout.findViewById(R.id.bookImage);
+            titleTextView = (TextView)bookLayout.findViewById(R.id.bookTitleTextView);
+            authorTextView = (TextView)bookLayout.findViewById(R.id.bookAuthorTextView);
+            //imageURL = "http://image.yes24.com/momo/TopCate1833/MidCate006/183255693.jpg";
+
+            book(Integer.toString(i));
+
+            bestList.addView(bookLayout);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            bookLayout = new InflatedBookLayout(getApplicationContext());
+            LinearLayout recommendList = (LinearLayout) findViewById(R.id.recommendList);
+            recommendList.addView(bookLayout);
+        }
+        //
+        //book();
     }
 
-    public void book() {
+    public void book(String num) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         RetrofitExService retrofitService = retrofit.create(RetrofitExService.class);
-        Call<Data> call = retrofitService.gettitle("1");
+        Call<Data> call = retrofitService.gettitle(num);
         call.enqueue(new Callback<Data>() {
             @Override
             public void onResponse(Call<Data> call, Response<Data> response) {
                 Data repo = response.body();
-                textView1.setText(repo.getTitle());
-                textView2.setText(repo.getAutor());
+                titleTextView.setText(repo.getTitle());
+                authorTextView.setText(repo.getAutor());
                 imageURL = repo.getImagesource();
-                //Log.i("qwer", "onResponse: "+repo.getImagesource());
+                Log.i("qwer", "onResponse: "+repo.getTitle());
             }
 
             @Override
@@ -84,14 +110,6 @@ public class MainActivity extends AppCompatActivity {
 
                     InputStream is = conn.getInputStream();
                     bitmap = BitmapFactory.decodeStream(is);
-
-                    /*handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            imageView.setImageBitmap(bitmap);
-                        }
-                    });
-                    imageView.setImageBitmap(bitmap);*/
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -103,9 +121,10 @@ public class MainActivity extends AppCompatActivity {
         try {
             mThread.join();
             Log.i("zxcv", "asdsgsgrd" + bitmap);
-            imageView.setImageBitmap(bitmap);
+            bookImageView.setImageBitmap(bitmap);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
     }
 }
